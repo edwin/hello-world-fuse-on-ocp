7977f82ff935f6d873f11d93c2cba2ca8685788c
edwin <edwinkun@gmail.com>
2021-08-13 17:01:55 +0700

    initial commit, still a :baby_chick:

---
+++ README.md
@@ -0,0 +1,10 @@
+Deploying Fuse to Openshift 4
+===================
+
+Tools
+-------------------
+* Programming Language - Java 8
+* Library - Red Hat Fuse 7.9
+
+How to Deploy
+------------------
---
+++ src/main/java/com/edw/routes/HelloWorldRoute.java
@@ -0,0 +1,30 @@
+package com.edw.routes;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+/**
+ * <pre>
+ *     com.edw.routes.HelloWorldRoute
+ * </pre>
+ *
+ * @author Muhammad Edwin < edwin at redhat dot com >
+ * 13 Agt 2021 15:53
+ */
+@Component
+public class HelloWorldRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        rest()
+                .get("hello")
+                .route()
+                .setHeader(Exchange.HTTP_RESPONSE_CODE, simple("200"))
+                .setHeader(Exchange.CONTENT_TYPE, simple("application/json"))
+                .setBody(constant("{\"world\":\"world\"}"))
+                .endRest()
+        ;
+    }
+
+}
